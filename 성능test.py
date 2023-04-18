e79d7c139747e325cf69969b272050a259d582b0
Byun Sunghyun <zzzsunghyun@gmail.com>
2023-04-18 15:46:43 +0900

    add_tester

---
+++ 성능test.py
@@ -0,0 +1,32 @@
+import time
+from sklearn.datasets import make_regression
+from xgboost import XGBRegressor
+
+def model_test(model_name, model):
+    x, y = make_regression(n_samples=100000, n_features=100)
+    
+    start_time = time.time()
+    model.fit(x, y)
+    end_time = time.time()
+    return f'{model_name}: 소요시간: {(end_time - start_time)} 초'
+
+xgb = XGBRegressor(n_estimators=1000, 
+                   learning_rate=0.01, 
+                   subsample=0.8, 
+                   colsample_bytree=0.8,
+                   objective='reg:squarederror', 
+                  )
+
+print(model_test('xgb (cpu)', xgb))
+
+xgb = XGBRegressor(n_estimators=1000, 
+                   learning_rate=0.01, 
+                   subsample=0.8, 
+                   colsample_bytree=0.8,
+                   objective='reg:squarederror', 
+                   tree_method='gpu_hist')
+
+print(model_test('xgb (gpu)', xgb))
+# 5600/3700
+# xgb (cpu): 소요시간: 174.36955738067627 초
+# xgb (gpu): 소요시간: 5.740361928939819 초
